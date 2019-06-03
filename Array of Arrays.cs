e623e8b893cc533f3f9adf1ae7c0626fa2a0a783
Ann Sakhno <sakhnyasha@gmail.com>
2019-06-03 18:35:45 +0300

    Draw a frame and a cross in it
    Draw a frame and a cross in it

---
+++ Array of Arrays.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Нарисовать рамочку, а внутри крест. Должно быть "масштабируемым"
+
+            //задаем размер
+            Console.WriteLine("Enter size (odd number):");
+            
+            int size = Int32.Parse(Console.ReadLine());
+
+            //задаем массив массивов
+            char[][] arr = new char[size][];
+
+            //заполнение * и " "
+            for (int i = 0; i < arr.Length; i++)
+            {
+                char[] temp = new char[size]; // чтобы можно было написать что-то типа char[][] arr = new char[size][size], но синтаксис не позволяет это написаить во вторые скобки
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if (i == 0 || i == arr.Length - 1 || j == 0 || j == temp.Length - 1 || (i == (arr.Length / 2) && j > 1 && j < temp.Length - 2) || (j == (temp.Length / 2) && i > 1 && i < arr.Length - 2))
+                    {
+                        temp[j] = '*';
+                    }
+                    else
+                    {
+                        temp[j] = ' ';
+                    }
+                }
+                arr[i] = temp;
+            }
+
+            Console.WriteLine();
+            //вывод массива массивов
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write(arr[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+            //чтобы консоль не закрывалась
+            Console.ReadKey();
+        }
+    }
+}
