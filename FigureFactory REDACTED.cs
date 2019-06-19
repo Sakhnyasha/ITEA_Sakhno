1ecf462104a7f9aaf510dbde0b68dd0dc186fef6
Ann Sakhno <sakhnyasha@gmail.com>
2019-06-19 19:10:44 +0300

    FigureFactory with inheritance
    FigureFactory with inheritance

--- FigureFactory REDACTED.cs
+++ FigureFactory REDACTED.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,83 +14,150 @@ namespace FigureFactory
     //"1. Вы выбрали *имя_фигуры*. Введите радиус/сторону/стороны(в зависимоти от метода подсчета периметра и площади треугольника):"
     //Должны вывестись параметры фигуры и меню.
     //Програма предлагает выбрать фигуру пока пользовательне нажмет выход.
-
-    //круг
     class Figure
-    {        
+    {
+        public string name;
         private string[] color_set = { "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Black", "White", "Pink", "Brown" };
         private string color;
         private double perimeter;
         private double square;
 
-        //вывод инфо о фигуре
-        public void printInfo()
+        virtual public string getName()
         {
-            Console.WriteLine("\nColor: " + color);
-            Console.WriteLine("Perimeter: " + perimeter);
-            Console.WriteLine("Square: " + square);
+            return "";
         }
-
-        //расчет периметра, площади. выбор цвета
-
-        //круг
-        public void setParametersCircle(int radius)
+        public string getColor()
         {
-            //если радиус больше 0
-            if (radius > 0)
+            if (color == null)
             {
                 Random rnd = new Random();
                 color = color_set[rnd.Next(color_set.Length - 1)];
-                perimeter = Math.Round((radius * 2 * Math.PI), 2);
-                square = Math.Round((Math.Pow(radius, 2) * Math.PI), 2);
-                printInfo(); //разобраться с ексепшенами и убрать!
             }
-            else
-            {
-                Console.WriteLine("\nEnter radius greater than 0!"); //тут должен быть ексепшн!
-            }
+            return color;
         }
-
-        //квадрат
-        public void setParametersSquare(int side)
+        virtual public double calculatePerimetr()
         {
-            if (side > 0)
-            {
-                Random rnd = new Random();
-                color = color_set[rnd.Next(color_set.Length - 1)];
-                perimeter = Math.Round(Convert.ToDouble(side * 4), 2);
-                square = Math.Round(Convert.ToDouble(Math.Pow(side, 2)), 2);
-                printInfo();//разобраться с ексепшенами и убрать!
-            }
-            else
-            {
-                Console.WriteLine("\nEnter side greater than 0!"); //тут должен быть ексепшн!
-            }
+            return 0;
         }
-
-        //треугольник
-        public void setParametersTriangle(int side_one, int side_two, int side_three)
+        virtual public double calculateSquare()
         {
-            //проверка на существование такой фигуры. Расчет по 3 сторонам
-            if ((side_one + side_two > side_three) && (side_one + side_three > side_two) && (side_two + side_three > side_one) && side_one > 0 && side_two > 0 && side_three > 0)
-            {
-                Random rnd = new Random();
-                color = color_set[rnd.Next(color_set.Length - 1)];
-                perimeter = Math.Round(Convert.ToDouble(side_one + side_two + side_three), 2);
-                square = Math.Round(Convert.ToDouble(Math.Sqrt(perimeter / 2 * (perimeter / 2 - side_one) * (perimeter / 2 - side_two) * (perimeter / 2 - side_three))), 2);
-                printInfo();//разобраться с ексепшенами и убрать!
-            }
-            else
-            {
-                Console.WriteLine("\nIt is not a Triangle!"); //тут должен быть ексепшн!
-            }
+            return 0;
+        }
+        virtual public void createFigure()
+        {
+        }
+        virtual public void printInfo()
+        {
+            Console.Clear();
+            Console.WriteLine("Name: " + getName() + "\nColor: " + getColor() + "\nPerimeter: " + calculatePerimetr() + "\nSquare: " + calculateSquare());
+        }
+    }       
+
+    //Круг
+    class Circle: Figure
+    {
+        double radius;
+        override public string getName()
+        {
+            return "Circle";
+        }
+        override public void createFigure()
+        {
+            Console.WriteLine("You chosed " + getName() + ". \nEnter Radius (ENTER key):");
+            radius = Convert.ToDouble(Program.ReadFromConsole().Trim());
+        }
+        override public double calculatePerimetr()
+        {
+            double perimeter = Math.Round((radius * 2 * Math.PI), 2);
+            return perimeter;
+        }
+        override public double calculateSquare()
+        {
+            double square = Math.Round((Math.Pow(radius, 2) * Math.PI), 2);
+            return square;
+        }        
+    }
+
+    //Квадрат
+    class Square: Figure
+    {
+        double side;
+        override public string getName()
+        {
+            return "Square";
+        }
+        override public void createFigure()
+        {
+            Console.WriteLine("You chosed " + getName() + ". \nEnter Side (ENTER key):");
+            side = Convert.ToDouble(Program.ReadFromConsole().Trim());
+        }
+        override public double calculatePerimetr()
+        {
+            double perimeter = Math.Round(Convert.ToDouble(side * 4), 2);
+            return perimeter;
+        }
+        override public double calculateSquare()
+        {
+            double square = Math.Round(Convert.ToDouble(Math.Pow(side, 2)), 2);
+            return square;
         }
     }
-    
+
+    //Треугольник
+    class Triangle: Figure
+    {
+        double side_one;
+        double side_two;
+        double side_three;
+
+        override public string getName()
+        {
+            return "Triangle";
+        }
+        override public void createFigure()
+        {
+            Console.WriteLine("You chosed " + getName() + ". \nEnter Side_1 (SPACEBAR key) Side_2 (SPACEBAR key) Side_3 (ENTER key):");
+            //парсинг строки
+            String[] arr = new String[0];
+            arr = Program.ReadFromConsole().TrimStart().TrimEnd().Split(' ');
+            while (arr.Length != 3)
+            {
+                Console.WriteLine("Enter 3 sides or remove extra spaces!");
+                arr = Program.ReadFromConsole().TrimStart().TrimEnd().Split(' ');
+            }
+            side_one = Convert.ToDouble(arr[0]);
+            side_two = Convert.ToDouble(arr[1]);
+            side_three = Convert.ToDouble(arr[2]);          
+        }
+
+        override public void printInfo()
+        {
+            if ((side_one + side_two > side_three) && (side_one + side_three > side_two) && (side_two + side_three > side_one) && side_one > 0 && side_two > 0 && side_three > 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Name: " + getName() + "\nColor: " + getColor() + "\nPerimeter: " + calculatePerimetr() + "\nSquare: " + calculateSquare());
+            }
+            else
+            {
+                Console.WriteLine("\nIt is not a Triangle!");
+            }
+        }
+        override public double calculatePerimetr()
+        {
+            double perimeter = Math.Round(Convert.ToDouble(side_one + side_two + side_three), 2);
+            return perimeter;
+        }
+        override public double calculateSquare()
+        {
+            double square = Math.Round(Convert.ToDouble(Math.Sqrt((side_one + side_two + side_three) / 2 * ((side_one + side_two + side_three) / 2 - side_one) * ((side_one + side_two + side_three) / 2 - side_two) * ((side_one + side_two + side_three) / 2 - side_three))), 2);
+            return square;
+        }
+    }
+
     class Program
     {
         //проверка на ввод только цифр, пробела и ентера!
-        static string ReadFromConsole()
+        static public string ReadFromConsole()
         {
             StringBuilder sb = new StringBuilder();
             while (true)
@@ -124,19 +191,21 @@ namespace FigureFactory
         static void Main(string[] args)
         {
             int button;
-            int counter = 1;
+            Figure figure = null;
 
             do
             {
+                //обнуление переменной
+                figure = null;
+
                 //меню
                 Console.WriteLine("\n==========MENU==========" +
                 "\nFor Circle press 1." +
                 "\nFor Square press 2." +
                 "\nFor Triangle press 3." +
                 "\nFor EXIT press 0.\n========================");
-
-                button = Convert.ToInt32(ReadFromConsole());
-                //counter++;
+                
+                button = Convert.ToInt32(ReadFromConsole());                 
                 Console.Clear();
 
                 //реализация меню
@@ -144,33 +213,15 @@ namespace FigureFactory
                 {
                     //для круга
                     case 1:
-                        Figure circle = new Figure();
-                        Console.WriteLine(counter + ". You chosed Circle. \nEnter Radius (ENTER key):");
-                        circle.setParametersCircle(Convert.ToInt32(ReadFromConsole().ToString().Trim()));
-                        counter++;
+                        figure = new Circle();                       
                         break;
                     //для квадрата
                     case 2:
-                        Figure square = new Figure();
-                        Console.WriteLine(counter + ". You chosed Square. \nEnter Side (ENTER key):");
-                        square.setParametersSquare(Convert.ToInt32(ReadFromConsole().ToString().Trim()));
-                        counter++;
+                        figure = new Square();
                         break;
                     //для треугольника
                     case 3:
-                        Figure triangle = new Figure();
-                        Console.WriteLine(counter + ". You chosed Triangle. \nEnter Side_1 (SPACEBAR key) Side_2 (SPACEBAR key) Side_3 (ENTER key):");
-                        //парсинг строки
-                        String[] arr = ReadFromConsole().ToString().TrimStart().TrimEnd().Split(' ');
-                        if (arr.Length == 3)
-                        {
-                            triangle.setParametersTriangle(Convert.ToInt32(arr[0]), Convert.ToInt32(arr[1]), Convert.ToInt32(arr[2]));
-                            counter++;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Enter 3 sides or remove extra spaces!");
-                        }
+                        figure = new Triangle();
                         break;
                     //выход из программы
                     case 0:
@@ -181,8 +232,16 @@ namespace FigureFactory
                         Console.WriteLine("Hmmmm... There's a trouble! Choose 1 or 2 or 3 or 0!");
                         break;
                 }
-            } while (button != 0);
+                
+                //вывод результата
+                if (figure != null)
+                {
+                    figure.createFigure();
+                    figure.printInfo(); //figure.calculatePerimetr(); figure.calculateSquare();
+                }                
 
+            } while (button != 0);
+            
             //чтобы консоль не закрылась
             Console.ReadKey();
         }
