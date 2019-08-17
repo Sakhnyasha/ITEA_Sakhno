fd2ae27d0ccbfc6a5a07bd4251ac0aad5dd5f7fd
Ann Sakhno <sakhnyasha@gmail.com>
2019-08-18 00:26:58 +0300

    Update Checker.cs
    Добавлены :
    -проверки полей формы на бэкенде и вывод ошибок 
    -поправлены формы регистрации и апдейта: легенда к полям
    -добавлена передача пола на радиобаттон

--- Final_Project/Final_Project/Checker.cs
+++ Final_Project/Final_Project/Checker.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Final_Project
 {
@@ -38,21 +39,113 @@ namespace Final_Project
         //добавление нового пользователя
         public string addAcc(string login, string name, string password, string rePassword, string address, int age, string sex)
         {
+            string validation = validate(login, name, password, rePassword, address, age, sex);
+
             if (userRepository.getUserByLogin(login) != null)
             {
-                return "This user already exists";
+                return "This user already exists!";
             }
-            if (password != rePassword) return "Passwords do not match";
-            userRepository.addUser(new User(name, login, GetHash(password), address, age, sex));
-            return "Access";
+
+            if (validation.Equals("COMPLETED SUCCESSFULLY!"))
+            {
+                userRepository.addUser(new User(name, login, GetHash(password), address, age, sex));
+                return "Access";
+            }
+            else return validation;
         }
 
         //обновление данных пользователя
         public string updateAcc(string login, string name, string password, string rePassword, string address, int age, string sex)
         {
-            if (password != rePassword) return "Passwords do not match";
-            userRepository.updateUser(new User(name, login, GetHash(password), address, age, sex));
-            return "Access";
+            string validation = validate(login, name, password, rePassword, address, age, sex);
+            if (validation.Equals("COMPLETED SUCCESSFULLY!"))
+            {
+                userRepository.updateUser(new User(name, login, GetHash(password), address, age, sex));
+                return "Access";
+            }
+            else return validation;
+        }
+
+        //фронт который стал бэком...
+        public bool isValidEmail(string login)
+        {
+            return !string.IsNullOrWhiteSpace(login) && new Regex(@"^([A-z0-9_\-\.]+)@(([a-z]{1,}\.)+)([a-z]{2,})$").IsMatch(login);
+        }
+        public bool isValidPass(string password)
+        {
+            return !string.IsNullOrWhiteSpace(password) && new Regex(@"^\w{8,12}$").IsMatch(password);
+        }
+        bool isValidName(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name) && new Regex(@"^[A-z]+(?: [A-z]+)*$").IsMatch(name);
+        }
+        bool isValidAddress(string address)
+        {
+            return !string.IsNullOrWhiteSpace(address) && new Regex(@"^\w+(?: \w+)*$").IsMatch(address);
+        }
+        bool isValidAge(int age)
+        {
+            return age >= 0 && age <= 200;
+        }
+        bool isValidSex(string sex)
+        {
+            return !string.IsNullOrWhiteSpace(sex) && (sex.Equals("Male") || sex.Equals("Female"));
+        }
+
+        String validate(string login, string name, string password, string rePassword, string address, int age, string sex)
+        {            
+            string errorValid = "REGISTRATION ERROR: <br>";
+            bool erorrFlag = false;
+            // login(email) check
+            if (!isValidEmail(login))
+            {
+                errorValid += "- Wrong type of LOGIN! It should be like: 'example@google.com';<br>";
+                erorrFlag = true;
+            }
+            // pass check
+            if (!password.Equals(rePassword))
+            {
+                errorValid += "- PASSWORD and RE-PASSWORD do not match!;<br>";
+                erorrFlag = true;
+            }            
+            if (!isValidPass(password) || !isValidPass(rePassword))
+            {
+                errorValid += "- Wrong PASSWORD OR RE-PASSWORD! Password length from 8 to 12 characters;<br>";                
+                erorrFlag = true;
+            }
+            // name check
+            if (!isValidName(name))
+            {
+                errorValid += "- Wrong type of NAME! It should be like: 'Anna' or 'Sakhno Anna'. NO SIGNS!;<br>";
+                erorrFlag = true;
+            }
+            // address check
+            if (!isValidAddress(address))
+            {
+                errorValid += "- Wrong type of ADDRESS! It should be like: 'Kyiv' or 'Kiev, Peremohy av. 93, ap. 56';<br>";
+                erorrFlag = true;
+            }
+            // age check
+            if (!isValidAge(age))
+            {
+                errorValid += "- Wrong AGE! It should be between 0 years and 200 years!;<br>";
+                erorrFlag = true;
+            }
+            //sex check
+            if (!isValidSex(sex))
+            {
+                errorValid += "- Wrong GENDER. It should be 'Male' or 'Female';<br>";
+                erorrFlag = true;
+            }
+            //message
+            if (erorrFlag)
+            {
+                return errorValid;
+            }
+            else
+            {
+                return "COMPLETED SUCCESSFULLY!";
+            }
         }
     }
 }
